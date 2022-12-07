ca686f55bae708dc94e03c84554fe87fd5a2bb4c
PaulBarron <paul.barron@attabotics.com>
2022-12-07 10:55:29 -0700

    Add properties for Recipe class.

--- Assignment4/Recipe.cs
+++ Assignment4/Recipe.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Assignment4
 {
+    /// <summary>
+    /// Every recipe has:
+    /// • a list of ingredients (strings)
+    /// • a description (instructions)
+    /// • a category as listed in the enum FoodCategory.
+    /// </summary>
     internal class Recipe
     {
+        private string[]? ingredients;
+        private string? instructions;
+        private FoodCategory category;
     }
 }
