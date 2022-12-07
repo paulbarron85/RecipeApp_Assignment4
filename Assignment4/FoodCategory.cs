da1667aef362364cb43359e217a9030339ea87d5
PaulBarron <paul.barron@attabotics.com>
2022-12-07 08:47:47 -0700

    Forgot to add files in previous commit, add enums for FoodCategory according to assignment.

---
+++ Assignment4/FoodCategory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    enum FoodCategory
+    {
+        Meats,
+        Pasta,
+        Pizza,
+        Fish,
+        Seafood,
+        Soups,
+        Stew,
+        Vegan,
+        Vegetarian,
+        Vegetarian_Dairy_Egg,
+        Other
+    }
+}
---
+++ Assignment4/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Assignment4
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
