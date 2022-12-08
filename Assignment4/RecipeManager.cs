eaee8e18c00c25d385f986adb0e3a2016236a7da
PaulBarron <paul.barron@attabotics.com>
2022-12-08 08:36:43 -0700

    Add methods for RecipeManager according to assignment instructions.

--- Assignment4/RecipeManager.cs
+++ Assignment4/RecipeManager.cs
@@ -8,5 +8,72 @@ namespace Assignment4
 {
     internal class RecipeManager
     {
+        private Recipe[] RecipeList;
+
+        public RecipeManager(int maxNumberOfElements)
+        {
+            RecipeList = new Recipe[maxNumberOfElements];  // Declares an array for storing recipes
+        }
+        public bool Add(Recipe recipe)
+        {
+            //check so recipe is not null before adding to the//recipList.
+            return false;
+        }
+        private bool Add(string name, FoodCategory category, string[] ingedients)
+        {
+            return false;
+        }
+
+        private void ChangeElement(int index, Recipe recipe)
+        {
+
+        }
+
+        private bool CheckIndex(int index)
+        {
+            return false;
+        }
+
+        private void DeleteElement(int index)
+        {
+
+        }
+
+        private int FindVacantPosition()
+        {
+            return 0;
+        }
+        
+        private int GetCurrentNumberOfRecipes()
+        {
+            return 0;
+        }
+        
+        private Recipe GetRecipeAt(int index)
+        {
+            Recipe recipe = null;
+
+            return recipe;
+        }
+        
+        private string[] RecipeListToString()
+        {
+            string[] recipeList_string = null;
+
+            return recipeList_string;
+        }
+        
+
+
+
+
+        private void MoveElementsOneStepToLeft(int index)
+        {
+            for (int i = index + 1; i < RecipeList.Length; i++)
+            {
+                RecipeList[i - 1] = RecipeList[i];
+                RecipeList[i] = null;
+            }
+        }
     }
 }
